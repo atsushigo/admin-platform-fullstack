d7dd7a6fa69fff2746822d6d19c57ce3e9c602d3
sam921611 <sam921611@kimo.com>
2022-01-12 11:47:27 +0800

    組件拆分
    App.vue 拆到router-view顯示路由對應
    
    拆分ListAticle出來
    
    router/index.js
    
    import ListArticle from "../views/ListArticle"
    import CreateArticle from "../views/CreateArticle"

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,43 +1,35 @@
 <template>
   <div id="app">
-     <el-container style="height: 100vh; border: 1px solid #eee">
-  <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-    <!-- 加router屬性 下面的index即可當路由跳轉 index="/posts/create" -->
-    <el-menu router :default-openeds="['1']">
-      <el-submenu index="1">
-        <template slot="title"><i class="el-icon-tickets"></i>內容管理</template>
-        <el-menu-item index="/posts/create">新建文章</el-menu-item>
-        <el-menu-item index="/posts">文章列表</el-menu-item>
-      </el-submenu>
+    <el-container style="height: 100vh; border: 1px solid #eee">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <!-- 加router屬性 下面的index即可當路由跳轉 index="/posts/create" -->
+        <el-menu router :default-openeds="['1']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-tickets"></i>內容管理</template>
+            <el-menu-item index="/articles/create">新建文章</el-menu-item>
+            <el-menu-item index="/articles/index">文章列表</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
 
-    </el-menu>
-  </el-aside>
-  
-  <el-container>
-    <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>查看</el-dropdown-item>
-          <el-dropdown-item>新增</el-dropdown-item>
-          <el-dropdown-item>删除</el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>王小虎</span>
-    </el-header>
-    
-    <el-main>
-      <el-table :data="tableData">
-        <el-table-column prop="date" label="日期" width="140">
-        </el-table-column>
-        <el-table-column prop="name" label="姓名" width="120">
-        </el-table-column>
-        <el-table-column prop="address" label="地址">
-        </el-table-column>
-      </el-table>
-    </el-main>
-  </el-container>
-</el-container>
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </el-header>
+
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -45,36 +37,27 @@
 /* import HelloWorld from './components/HelloWorld.vue' */
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     /* HelloWorld */
   },
-   data() {
-      const item = {
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      };
-      return {
-        tableData: Array(20).fill(item)
-      }
-    }
-}
+};
 </script>
 
 <style>
-html, body{
-  padding:0;
-  margin:0;
+html,
+body {
+  padding: 0;
+  margin: 0;
 }
 
- .el-header {
-    background-color: #B3C0D1;
-    color: #333;
-    line-height: 60px;
-  }
-  
-  .el-aside {
-    color: #333;
-  }
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
 </style>
---
+++ frontend/src/views/CreateArticle.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>create article</div>
+</template>
+
+<script>
+export default {
+  /* name: {
+    CreateArticle,
+  }, */
+};
+</script>
+
+<style></style>
