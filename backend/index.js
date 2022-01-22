a010850d667b2829bffbf1ade2f43944357613ee
sam921611 <sam921611@kimo.com>
2022-01-22 14:03:12 +0800

    增加後端article post接口

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const app = express()
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/admin-platform", {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useCreateIndex: true,
+})
+
+/* 建表 */
+const Article = mongoose.mode("Artice", new mongoose.Schema({
+    title: { type: String },
+    body: { type: String },
+}))
+
+app.use(require("cors")())
+/* 識別客戶端傳過來的json */
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+    res.send("good")
+})
+
+app.post("/api/articles", async (req, res) => {
+    const article = await Article.create(req.body)
+    res.send(article)
+})
+
+app.listen(3001, () => {
+    console.log("listening good")
+})
